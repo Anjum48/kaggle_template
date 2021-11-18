962f7bf9370fb9217a491bf36eae94ac7cc1c676
Anjum Sayed <anjum.sayed48@gmail.com>
2021-11-18 21:33:20 +0000

    Storage drive option

--- src/config.py
+++ src/config.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
 COMP_NAME = ""
+STORAGE = "storage_dimm2"
+# STORAGE = "storage"
 
-INPUT_PATH = Path(f"/mnt/storage_dimm2/kaggle_data/{COMP_NAME}/")
-OUTPUT_PATH = Path(f"/mnt/storage_dimm2/kaggle_output/{COMP_NAME}/")
+INPUT_PATH = Path(f"/mnt/{STORAGE}/kaggle_data/{COMP_NAME}/")
+OUTPUT_PATH = Path(f"/mnt/{STORAGE}/kaggle_output/{COMP_NAME}/")
 CONFIG_PATH = Path(f"/home/anjum/kaggle/{COMP_NAME}/hyperparams.yml")
 MODEL_CACHE = Path("/mnt/storage/model_cache/torch")
 # MODEL_CACHE = Path("/mnt/storage/model_cache/huggingface")
